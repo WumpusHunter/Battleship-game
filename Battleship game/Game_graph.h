47a24ebea6446c8558abdd7c59440799ba6f8649
kovalevskioleg@gmail.com <kovalevskioleg@gmail.com>
2020-09-04 06:51:09 +0400

    Implement a game field

---
+++ Battleship game/Game_graph.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "GraphicsLib/Graph.h"
+
+//------------------------------------------------------------------------------
+
+namespace Graph_lib {
+
+	//------------------------------------------------------------------------------
+
+	// Invariant: cell_w >= 0, cell_h >= 0, h_num >= 0, v_num >= 0
+	class Marked_grid : public Grid {
+	public:
+		// Construction
+		Marked_grid(Point xy, unsigned int cell_w, unsigned int cell_h,
+			unsigned int h_num, unsigned int v_num, const std::string& mark);
+
+		// Drawing of shape
+		void draw_lines() const;
+
+		// Access to parameters (writing)
+		void set_color(Color c);
+
+	private:
+		Marks lab;		// Label (marks)
+	};
+
+	//------------------------------------------------------------------------------
+
+}
+
+//------------------------------------------------------------------------------
