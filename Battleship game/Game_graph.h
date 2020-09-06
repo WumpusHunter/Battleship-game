2fe103474fdef04f91919813bdc28e832a2469c3
kovalevskioleg@gmail.com <kovalevskioleg@gmail.com>
2020-09-06 06:16:22 +0400

    Implement a game ship

--- Battleship game/Game_graph.h
+++ Battleship game/Game_graph.h
@@ -21,7 +21,38 @@ namespace Graph_lib {
 		void set_color(Color c);
 
 	private:
-		Marks lab;		// Label (mark)
+		Marks lab;		// Label (marks)
+	};
+
+	//------------------------------------------------------------------------------
+
+	// Invariant: cell_w >= 0, cell_h >= 0
+	class Ship : public Shape {
+	public:
+		enum class Kind {		// Kinds of shape and their length in cells
+			Torpedo_boat = 1, Destroyer = 2, Cruiser = 3, Battleship = 4
+		};
+		enum class Orientation {
+			horizontal, vertical
+		};
+
+		// Construction
+		Ship(Point xy, unsigned int cell_w, unsigned int cell_h, Kind k, Orientation o);
+
+		// Drawing of shape
+		void draw_lines() const;
+
+		// Access to parametrs (writing)
+		void rotate();
+		void set_color(Color c);
+		void set_fill_color(Color c);
+		void set_style(Line_style ls);
+		void move(int dx, int dy);
+
+	private:
+		Vector_ref<Rectangle> cells;
+		Kind kind;
+		Orientation orient;
 	};
 
 	//------------------------------------------------------------------------------
--- Battleship game/Source.cpp
+++ Battleship game/Source.cpp
@@ -12,17 +12,29 @@ int main()
 {
     // Window with top-left angle at (100, 100), of size 600 * 400, labeled "Window"
     Simple_window win{ Point{ 100, 100 }, 600, 400, "Window" };
-    // Marked grid with top-left angle of grid at (150, 150), of size 50 * 50 for
-    // each cell, with 6 vertical and 3 horintal lines, marked with "123ABCDEF"
-    Marked_grid mgrid{ Point{ 100, 100 }, 50, 50, 10, 10, "0123456789ABCDEFGHIJ" };
+    // Marked grid with top-left angle of grid at (50, 50), of size 50 * 50 for each
+    // cell, with 10 * 6 vertical and horintal lines, marked with "01234ABCDE"
+    Marked_grid mgrid{ Point{ 50, 50 }, 50, 50, 10, 6, "012345ABCDEFGHIJ" };
+    // Ship with top-left angle of head at (50, 50), of 50 * 50 size
+    // for each cell, of kind Cruiser, and oriented horintally
+    Ship ship{ Point{ 50, 50 }, 50, 50, Ship::Kind::Cruiser, Ship::Orientation::vertical };
 
     // Modify parameters of graphics
     mgrid.set_color(Color::Color_type::black);
     mgrid.set_fill_color(Color::Color_type::blue);
+    ship.set_color(Color::Color_type::black);
+    ship.set_fill_color(Color::Color_type::cyan);
 
     // Connection of graphics with window
     win.attach(mgrid);
+    win.attach(ship);
 
+    win.wait_for_button();
+    ship.move(50, 50);
+    win.wait_for_button();
+    ship.rotate();
+    win.wait_for_button();
+    ship.rotate();
     win.wait_for_button();
 }
 
